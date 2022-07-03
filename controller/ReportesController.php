0d43642d5b61901a4fd248bfa2eb5bcfc0643360
Darioorfano <darioorfano@hotmail.com>
2022-07-03 00:07:35 -0300

    Se realizo la insercion de estadisticas para tasa de ocupacion y facturacion mensual, ademas se realizo las funciones para exportar PDF con su respectiva data en cada seccion de reportes

--- controller/ReportesController.php
+++ controller/ReportesController.php
@@ -4,11 +4,14 @@ class ReportesController
 {
     private $printer;
     private $reporteModel;
+    private $pdfHelper;
+
    
-    public function __construct($printer,$reporteModel)
+    public function __construct($printer,$reporteModel,$pdfHelper)
     {
         $this->printer = $printer;
         $this->reporteModel=$reporteModel;
+        $this->pdfHelper=$pdfHelper;
     }
     public function execute() {
         $data=array();
@@ -34,12 +37,63 @@ class ReportesController
           if(isset($_SESSION["nombre"])){
               $data["nombre"]=$_SESSION["nombre"];
           }
+          /*Se genero el array, para separar la data correspondiente,y las etiquetas(Mes,Facturacion Total)
+          para el grafico de Barras*/
+          $facturacionMensualData=array();
+          $facturacionMensualLabel=array();
           
+          
+          foreach($facturacionMensual as $facturaMes){
+            array_push($facturacionMensualData,$facturaMes['Facturacion Total']);
+            array_push( $facturacionMensualLabel,$facturaMes['Mes']);
+
+          }
+          //Convierto el Array en String para manipularlo con Javascript
+          $data['FacturacionData']=implode(',',$facturacionMensualData);
+          $data['FacturacionLabel']=implode(',',$facturacionMensualLabel);
+          
+          
+          
+          $tasaOcupacionData=array();
+          $tasaOcupacionLabel=array();
+
+          foreach($tasaOcupacion as $tasa){
+            array_push($tasaOcupacionData,$tasa['Asientos Ocupados']);
+            array_push($tasaOcupacionLabel,$tasa['id_viaje']);
+           
+          }
+          $data['TasaOcupacionData']=implode(',',$tasaOcupacionData);
+          $data['TasaOcupacionLabel']=implode(',',$tasaOcupacionLabel);
+
+
           $this->printer->generateView('reporteView.html', $data);
 
 
     }
-     
+     public function exportarPDF(){
+        $tipoReporte=$_GET['tipoReporte'];
+
+        if($tipoReporte == 'cabinaMasVendida'){
+
+            $cabinaMasVendida=$this->reporteModel->getCabinaMasVendida();
+            $this->pdfHelper->generarPDFParaCabinaMasVendida($cabinaMasVendida);
+        }else if($tipoReporte == 'facturacionMensual'){
+            $facturacionMensual=$this->reporteModel->getFacturacionMensual();
+            $this->pdfHelper->generarPDFParaFacturacionMensual($facturacionMensual);
+        }else if($tipoReporte == 'facturaCliente'){
+            $facturaCliente=$this->reporteModel->getFacturaCliente();
+            $this->pdfHelper->generarPDFParaFacturacionCliente($facturaCliente);
+
+        }else if($tipoReporte == 'tasaDeOcupacion'){
+            $tasaOcupacion=$this->reporteModel->getTasaOcupacion();
+            $this->pdfHelper->generarPDFParaTasaDeOcupacionPorViaje($tasaOcupacion);
+
+        }else{
+            header("location: /reportes");
+            exit();
+        }
+       
+     }
 
 }
 
--- helper/PDFHelper.php
+++ helper/PDFHelper.php
@@ -72,5 +72,91 @@ class PDFHelper
 
         $pdf->Output('D', 'Pase-de-abordaje.pdf');
     }
+    public function generarPDFParaTasaDeOcupacionPorViaje($tasaDeOcupacion){
+        $pdf = new FPDF();
+    
+        $pdf->AddPage();
+        $pdf->SetFont('Arial','B',15);
+        $pdf->Image('public/images/Cohete.png',80,8,33);
+        $pdf->Ln(10);
+        // Arial bold 15
+ 
+        $pdf->Cell(80,20,'Tasa de Ocupacion Por Viaje',0,'C');
+        $pdf->Ln(20);
+        foreach ($tasaDeOcupacion as $tasa) {
+            $pdf->Cell(80,20,$tasa['id_viaje'],0,'C');
+            $pdf->Cell(80,20,$tasa['Asientos Ocupados'],0,'C');
+            $pdf->Cell(80,20,$tasa['Asientos Totales'],0,'C');
+            $pdf->Cell(80,20,$tasa['Tasa'],0,'C');
+        }
+       
+        $pdf->Output('D', 'Tasa-De-Ocupacion-Por-Viaje.pdf');
+    }
+
+
+
+    
+    public function generarPDFParaFacturacionMensual($facturacionMensual){
+        $pdf = new FPDF();
+    
+        $pdf->AddPage();
+        $pdf->SetFont('Arial','B',15);
+        $pdf->Image('public/images/Cohete.png',80,8,33);
+        $pdf->Ln(10);
+        // Arial bold 15
+ 
+        $pdf->Cell(80,20,'Facturacion Mensual',0,'C');
+        $pdf->Ln(20);
+        foreach ($facturacionMensual as $facturaMes) {
+            $pdf->Cell(80,20,$facturaMes['Mes'],0,'C');
+            $pdf->Cell(80,20,$facturaMes['Facturacion Total'],0,'C');
+        }
+       
+        $pdf->Output('D', 'Facturacion-Mensual.pdf');
+    }
+
+    public function generarPDFParaFacturacionCliente($facturacionCliente){
+        $pdf = new FPDF();
+    
+        $pdf->AddPage();
+        $pdf->SetFont('Arial','B',15);
+        $pdf->Image('public/images/Cohete.png',80,8,33);
+        $pdf->Ln(10);
+        // Arial bold 15
+ 
+        $pdf->Cell(80,20,'Facturacion Mensual',0,'C');
+        $pdf->Ln(20);
+        foreach ($facturacionCliente as $facturaCliente) {
+
+
+            $pdf->Cell(80,20,$facturaCliente['id_usuario'],0,'C');
+            $pdf->Cell(80,20,$facturaCliente['Facturacion Cliente'],0,'C');
+            $pdf->Cell(80,20,$facturaCliente['nombre'],0,'C');
+            $pdf->Cell(80,20,$facturaCliente['apellido'],0,'C');
+            $pdf->Cell(80,20,$facturaCliente['dni'],0,'C');
+        }
+       
+        $pdf->Output('D', 'Facturacion-Cliente.pdf');
+    }
+
+
+
+    public function generarPDFParaCabinaMasVendida($datosCabinaMasVendida){
+        $pdf = new FPDF();
+    
+        $pdf->AddPage();
+        $pdf->SetFont('Arial','B',15);
+        $pdf->Image('public/images/Cohete.png',80,8,33);
+        $pdf->Ln(10);
+        // Arial bold 15
+ 
+        $pdf->Cell(80,20,'Cabina Mas Vendida',0,'C');
+        $pdf->Ln(20);
+        $pdf->Cell(80,20,$datosCabinaMasVendida['descripcion'],0,'C');
+        $pdf->Cell(80,20,$datosCabinaMasVendida['Cantidad'],0,'C');
+        $pdf->Output('D', 'Cabinas-Mas-Vendida.pdf');
+    }
+   
+    
 
 }
